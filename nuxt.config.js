5771ca367fe7c9699cc5ae994bd90378d30c3683
Jayden <jayden.shaw@gmail.com>
2022-03-23 23:22:55 -0600

    Install composition-api

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/vue-composition-api'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -41,6 +41,12 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    breakpoint: {
+      xs: 575,
+      sm: 768,
+      md: 992,
+      lg: 1200,
+    },
     theme: {
       dark: true,
       themes: {
---
+++ plugins/vue-composition-api.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+import vueCompositionApi from '@vue/composition-api'
+
+Vue.use(vueCompositionApi)
