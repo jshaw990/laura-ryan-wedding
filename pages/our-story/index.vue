84699fa611a337ad120a14153d5d3f0ca0e7d660
Jayden <jayden.shaw@gmail.com>
2022-03-24 00:29:23 -0600

    Our story page.

---
+++ pages/our-story/index.vue
@@ -0,0 +1,142 @@
+<template>
+  <v-col cols="12" md="10" class="page m-8 px-2">
+    <div class="ma-8 px-2 small-title background-hue">Our Story</div>
+    <v-col cols="12" class="mx-auto mb-12">
+      <v-card class="px-8 py-8 our-story" color="rgba(92, 20, 39, .75)">
+        <div
+          class="my-6 d-flex flex-column flex-md-row justify-space-between align-center"
+        >
+          <p>
+            Ryan and Laura first met online in 2018. Actually, they first
+            matched online 3 years prior, but they never ended up meeting in
+            person. December 2018, they found each other again. This time a date
+            was planned, and they met for dinner at Original Joe’s on Macleod
+            Trail. Ryan was nervous but excited to meet Laura and she felt the
+            same. They chatted and got to know each other. The conversations
+            went very well, it was easy to talk to one another, and the whole
+            evening was fun and relaxed. Laura knew right away that something
+            just felt right, also Ryan found her to be very cute and was super
+            interested in hanging out again. The very next day Laura originally
+            had plans to go to a Christmas market with her friend Flo (Maid of
+            Honour); but something came up and Flo couldn’t make it, well Ryan
+            swoops in and offers to join her instead and they had another
+            amazing night walking around the Christmas market downtown then
+            going for dinner at Famoso’s Pizza. Ryan was hooked! They were
+            creating an amazing connection and getting along so well. They
+            couldn’t stop talking to each other every day. Their third date was
+            at Zoo Lights at the Calgary Zoo, they had a blast and have made it
+            their tradition to go every year on the day they first met! 12.06.18
+          </p>
+          <v-img
+            src="https://i.imgur.com/SHoB7Ms.jpg"
+            contain
+            max-width="215"
+            class="mx-md-3 bg-white"
+          />
+        </div>
+        <div
+          class="my-6 d-flex flex-column-reverse flex-md-row justify-space-between align-center"
+        >
+          <v-img
+            src="https://i.imgur.com/0u40mp3.jpg"
+            contain
+            max-width="215"
+            class="mx-md-3 bg-white"
+          />
+          <p>
+            Laura ended up meeting Ryan’s family pretty fast in an accidental
+            kind of way. His family goes to Heritage Park’s Once Upon A
+            Christmas festival for brunch every year, but this year his parents
+            couldn't make it so they asked if Laura wanted to go with Ryan, his
+            brother, Scott and his future sister in law Kat instead (Best Man
+            and Bridesmaid). Well, the night before the brunch Ryan’s parents
+            changed their minds and chose to join in and they got to meet Laura
+            on their fourth date. The day went very smoothly, Laura and Ryan’s
+            family got along very well. Everything about Ryan felt so right to
+            Laura!
+          </p>
+        </div>
+        <div
+          class="my-6 d-flex flex-column flex-md-row justify-space-between align-center"
+        >
+          <p>
+            Fast Forward to May 2019, Ryan took Laura to his house in Sylvan
+            Lake. He had an enjoyable weekend and was trying to make it fun for
+            the both of them as he wasn’t feeling the greatest. They rode their
+            bikes around town, stopped at a brewery, and played some board
+            games. Little did she know that as she was taking a picture, she had
+            her game board in full view showing him where all her ships were on
+            her board. Luckily Ryan didn’t notice until after they finished the
+            game and both had a good laugh over it! Once back at the house Ryan
+            chose to rest a bit so he could feel better and Laura took the dogs
+            for a walk around the lake. Little did she know then, but that was
+            the day Ryan knew he fell in love! 05.20.19
+          </p>
+          <v-img
+            src="https://i.imgur.com/EhPJjms.jpg"
+            contain
+            max-width="215"
+            class="mx-md-3 bg-white"
+          />
+        </div>
+      </v-card>
+    </v-col>
+    <div class="ma-8 px-2 small-title background-hue">Our Proposal</div>
+    <v-col cols="12" class="mx-auto mb-12">
+      <v-card class="px-8 py-8 our-proposal" color="rgba(92, 20, 39, .75)">
+        <div
+          class="my-6 d-flex flex-column flex-md-row justify-space-between align-center"
+        >
+          <p>
+            Laura and Ryan had gone to Sylvan Lake with Scott and Kat to build a
+            deck for the house. The first night they were there Ryan suggested
+            they all take the dogs for a walk by the water. Earlier in the day,
+            Beth (Laura’s sister and Bridesmaid) asked Laura if she thought Ryan
+            might propose this weekend. Well of course she said, “No, I don’t
+            think he even has got a ring yet.” Well on their walk Laura
+            mentioned this conversation to Ryan, Scott, and Kat and they all
+            laughed about it and didn’t think much of it after that. Meanwhile,
+            Ryan had the ring in his pocket this whole time and once they got
+            down to the water, Ryan got down on one knee and asked Laura to
+            marry him. She felt so happy, loved, and surprised. Of course, she
+            said YES!!! It was such a wonderful and perfect moment at sunset,
+            overlooking the water and the lighthouse!
+          </p>
+          <v-img
+            src="https://i.imgur.com/AOBam8K.jpg"
+            contain
+            max-width="215"
+            class="mx-md-3 bg-white"
+          />
+        </div>
+        <div class="my-6 d-flex flex-column justify-space-between align-center">
+          <p>
+            It was 2 years to the day after Ryan knew he had fallen in love with
+            Laura! 05.20.21. She was so glad Scott and Kat could be there to
+            celebrate this moment with them and capture a picture too! Ryan says
+            “It was one of the most nerve-racking but happiest days of my life
+            so far and I will forever be happy I asked Laura to be my wife.”
+          </p>
+          <v-img
+            src="https://i.imgur.com/X1TjWqA.jpg"
+            contain
+            max-width="215"
+            class="d-block d-md-none mx-md-3 bg-white"
+          />
+          <v-img
+            src="https://i.imgur.com/X1TjWqA.jpg"
+            contain
+            max-width="450"
+            class="d-none d-md-block mx-md-3 bg-white"
+          />
+        </div>
+      </v-card>
+    </v-col>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: 'OurStory',
+}
+</script>
