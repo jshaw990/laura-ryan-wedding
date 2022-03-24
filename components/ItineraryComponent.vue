99023463c8d0615f56faffec299edf4940fb9b0c
Jayden <jayden.shaw@gmail.com>
2022-03-24 00:04:29 -0600

    Itinerary component and page.

---
+++ components/ItineraryComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-timeline
+    align-top
+    class="mx-md-4 my-md-8 mr-2"
+    color="rgba(0,0,0,0)"
+    :dense="$vuetify.breakpoint.mdAndDown"
+  >
+    <v-timeline-item
+      v-for="item in itinerary"
+      :key="item.index"
+      color="white"
+      fill-dot
+      small
+      class="my-md-n10"
+    >
+      <v-card color="rgba(92, 20, 39, 0.75)">
+        <v-card-title>
+          {{ item.time }}
+        </v-card-title>
+        <v-card-text>
+          {{ item.content }}
+          <div v-if="item.add" class="text-caption" v-html="item.add" />
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+</template>
+
+<script>
+export default {
+  name: 'ItineraryComponent',
+  props: ['itinerary'],
+}
+</script>
---
+++ pages/itinerary/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-col cols="12" md="8" class="mx-auto page">
+    <div class="m-8 small-title background-hue">Day-of Itinerary</div>
+    <v-card color="rgba(0,0,0,0)">
+      <ItineraryComponent :itinerary="itinerary" />
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+import { reactive, toRefs } from '@vue/composition-api'
+
+import itinerary from '@/static/itinerary.json'
+
+import ItineraryComponent from '~/components/ItineraryComponent.vue'
+
+export default {
+  name: 'Itinerary',
+  components: {
+    ItineraryComponent,
+  },
+
+  setup() {
+    const state = reactive({
+      itinerary,
+    })
+
+    return {
+      ...toRefs(state),
+    }
+  },
+}
+</script>
